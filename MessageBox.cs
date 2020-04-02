bbbb6bb6dbfc33e43e21028c0ebd319bcccfa618
Nicholas10128 <46949284+Nicholas10128@users.noreply.github.com>
2020-04-02 22:30:59 +0800

    add message box.

---
+++ MessageBox.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GCT
+{
+    namespace UI
+    {
+        public class MessageBox
+        {
+            public enum Type
+            {
+                ConfirmOnly,
+                YesOrNo,
+                YesOrNoOrCancel,
+                NeverDisplayAgain
+            }
+
+            public enum ButtonID
+            {
+                Confirm,
+                Cancel,
+                Close
+            }
+
+            public delegate void Callback(ButtonID bid, object parameter);
+
+            public class MessageInfo
+            {
+                public Type m_Type;
+                public string m_Caption;
+                public string m_Content;
+                public TextAnchor m_ContentAlignment;
+                public Callback m_Callback;
+                public object m_Parameter;
+                public byte m_AutoClose;
+
+                public bool m_Destroying;
+                public float m_BeginTime;
+            }
+
+            private static Queue<MessageInfo> m_MessageQueue = new Queue<MessageInfo>();
+            private static MessageInfo m_CurrentMessage = null;
+            private static Canvas m_MessageBoxUI = null;
+
+            public static void Initialize()
+            {
+                GameObject go = GameObject.Find("MessageBox");
+                m_MessageBoxUI = go.GetComponent<Canvas>();
+            }
+
+            public static void Show(string caption, string content, Type type, TextAnchor contentAlignment, Callback callback, object parameter)
+            {
+                MessageInfo mi = new MessageInfo();
+                mi.m_Caption = caption;
+                mi.m_Content = content;
+                mi.m_Type = type;
+                mi.m_ContentAlignment = contentAlignment;
+                mi.m_Callback = callback;
+                mi.m_Parameter = parameter;
+                mi.m_AutoClose = 0;
+                mi.m_Destroying = false;
+                mi.m_BeginTime = 0;
+                m_MessageQueue.Enqueue(mi);
+            }
+
+            public static void Show(string caption, string content, TextAnchor contentAlignment, Callback callback, object parameter, byte autoCloseSeconds)
+            {
+                MessageInfo mi = new MessageInfo();
+                mi.m_Caption = caption;
+                mi.m_Content = content;
+                mi.m_Type = Type.ConfirmOnly;
+                mi.m_ContentAlignment = contentAlignment;
+                mi.m_Callback = callback;
+                mi.m_Parameter = parameter;
+                mi.m_AutoClose = autoCloseSeconds;
+                mi.m_Destroying = false;
+                mi.m_BeginTime = 0;
+                m_MessageQueue.Enqueue(mi);
+            }
+
+            public static void Tick()
+            {
+                if (m_CurrentMessage != null && !m_CurrentMessage.m_Destroying)
+                {
+                    if (m_CurrentMessage.m_AutoClose > 0)
+                    {
+                        float elapsed = Time.time - m_CurrentMessage.m_BeginTime;
+                        if (elapsed >= m_CurrentMessage.m_AutoClose)
+                        {
+                            m_MessageBoxUI.SendMessage("OnConfirmButtonClick");
+                        }
+                    }
+                    return;
+                }
+
+                if (m_MessageQueue.Count > 0)
+                {
+                    m_CurrentMessage = m_MessageQueue.Dequeue();
+                    m_MessageBoxUI.enabled = true;
+                    m_MessageBoxUI.SendMessage("OnOpen", m_CurrentMessage);
+                }
+            }
+        }
+    }
+}
---
+++ MessageBoxUI.cs
@@ -0,0 +1,172 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GCT
+{
+    namespace UI
+    {
+        public class MessageBoxUI : MonoBehaviour
+        {
+            public Text m_Caption;
+            public Text m_Content;
+
+            public GameObject ConfirmOnlyGroup;
+            public GameObject YesOrNoGroup;
+            public GameObject YesOrNoOrCancelGroup;
+            public GameObject NeverDisplayAgainGroup;
+
+            public GameObject m_ConfirmOnlyAutoCloseSeconds;
+            public GameObject m_ConfirmOnlyAutoCloseDescription;
+            public GameObject m_YesOrNoAutoCloseSeconds;
+            public GameObject m_YesOrNoAutoCloseDescription;
+
+            private StringBuilder m_StringBuilder = new StringBuilder();
+
+            private Text m_ConfirmOnlyAutoCloseSecondsText;
+            private Text m_YesOrNoAutoCloseSecondsText;
+            private byte m_LastLeftSeconds;
+
+            private MessageBox.MessageInfo m_MessageInfo;
+            private Canvas m_Canvas;
+
+
+            void Start()
+            {
+                m_Caption.text = string.Empty;
+                m_Content.text = string.Empty;
+                ConfirmOnlyGroup.SetActive(false);
+                YesOrNoGroup.SetActive(false);
+                YesOrNoOrCancelGroup.SetActive(false);
+                NeverDisplayAgainGroup.SetActive(false);
+                m_ConfirmOnlyAutoCloseSecondsText = m_ConfirmOnlyAutoCloseSeconds.GetComponent<Text>();
+                m_YesOrNoAutoCloseSecondsText = m_YesOrNoAutoCloseSeconds.GetComponent<Text>();
+                m_Canvas = GetComponent<Canvas>();
+            }
+
+            void Update()
+            {
+                if (m_MessageInfo != null && !m_MessageInfo.m_Destroying)
+                {
+                    if (m_MessageInfo.m_AutoClose > 0)
+                    {
+                        Text autoCloseSecondsText = null;
+                        switch (m_MessageInfo.m_Type)
+                        {
+                            case MessageBox.Type.ConfirmOnly:
+                                autoCloseSecondsText = m_ConfirmOnlyAutoCloseSecondsText;
+                                break;
+                            case MessageBox.Type.YesOrNo:
+                                autoCloseSecondsText = m_YesOrNoAutoCloseSecondsText;
+                                break;
+                            default:
+                                Debug.LogError("Fatal error! m_MessageInfo.m_Type = " + m_MessageInfo.m_Type);
+                                return;
+                        }
+                        float elapsed = Time.time - m_MessageInfo.m_BeginTime;
+                        byte leftSeconds = (byte)Mathf.CeilToInt(m_MessageInfo.m_AutoClose - elapsed);
+                        if (m_LastLeftSeconds != leftSeconds)
+                        {
+                            m_StringBuilder.Append(m_LastLeftSeconds = leftSeconds);
+                            autoCloseSecondsText.text = m_StringBuilder.ToString();
+                            m_StringBuilder.Clear();
+                        }
+                    }
+                }
+            }
+
+            public void SetContentAlignment(TextAnchor alignment)
+            {
+                m_Content.alignment = alignment;
+            }
+
+            public void OnOpen(MessageBox.MessageInfo mi)
+            {
+                m_MessageInfo = mi;
+                m_Caption.text = m_MessageInfo.m_Caption;
+                m_Content.text = m_MessageInfo.m_Content;
+                m_Content.alignment = m_MessageInfo.m_ContentAlignment;
+
+                switch (m_MessageInfo.m_Type)
+                {
+                    case MessageBox.Type.ConfirmOnly:
+                        ConfirmOnlyGroup.SetActive(true);
+                        YesOrNoGroup.SetActive(false);
+                        YesOrNoOrCancelGroup.SetActive(false);
+                        NeverDisplayAgainGroup.SetActive(false);
+                        if (m_MessageInfo.m_AutoClose == 0)
+                        {
+                            m_ConfirmOnlyAutoCloseSeconds.SetActive(false);
+                            m_ConfirmOnlyAutoCloseDescription.SetActive(false);
+                        }
+                        else
+                        {
+                            m_ConfirmOnlyAutoCloseSeconds.SetActive(true);
+                            m_ConfirmOnlyAutoCloseDescription.SetActive(true);
+                            m_StringBuilder.Append(m_LastLeftSeconds = m_MessageInfo.m_AutoClose);
+                            m_ConfirmOnlyAutoCloseSecondsText.text = m_StringBuilder.ToString();
+                            m_StringBuilder.Clear();
+                            m_MessageInfo.m_BeginTime = Time.time;
+                        }
+                        break;
+                    case MessageBox.Type.YesOrNo:
+                        ConfirmOnlyGroup.SetActive(false);
+                        YesOrNoGroup.SetActive(true);
+                        YesOrNoOrCancelGroup.SetActive(false);
+                        NeverDisplayAgainGroup.SetActive(false);
+                        if (m_MessageInfo.m_AutoClose == 0)
+                        {
+                            m_YesOrNoAutoCloseSeconds.SetActive(false);
+                            m_YesOrNoAutoCloseDescription.SetActive(false);
+                        }
+                        else
+                        {
+                            m_YesOrNoAutoCloseSeconds.SetActive(true);
+                            m_YesOrNoAutoCloseDescription.SetActive(true);
+                            m_StringBuilder.Append(m_LastLeftSeconds = m_MessageInfo.m_AutoClose);
+                            m_YesOrNoAutoCloseSecondsText.text = m_StringBuilder.ToString();
+                            m_StringBuilder.Clear();
+                            m_MessageInfo.m_BeginTime = Time.time;
+                        }
+                        break;
+                    case MessageBox.Type.YesOrNoOrCancel:
+                        ConfirmOnlyGroup.SetActive(false);
+                        YesOrNoGroup.SetActive(false);
+                        YesOrNoOrCancelGroup.SetActive(true);
+                        NeverDisplayAgainGroup.SetActive(false);
+                        break;
+                    case MessageBox.Type.NeverDisplayAgain:
+                        ConfirmOnlyGroup.SetActive(false);
+                        YesOrNoGroup.SetActive(false);
+                        YesOrNoOrCancelGroup.SetActive(false);
+                        NeverDisplayAgainGroup.SetActive(true);
+                        break;
+                }
+            }
+
+            public void OnConfirmButtonClick()
+            {
+                m_MessageInfo.m_Callback?.Invoke(MessageBox.ButtonID.Confirm, m_MessageInfo.m_Parameter);
+                OnClose();
+            }
+
+            public void OnCancelButtonClick()
+            {
+                m_MessageInfo.m_Callback?.Invoke(MessageBox.ButtonID.Cancel, m_MessageInfo.m_Parameter);
+                OnClose();
+            }
+
+            public void OnCloseButtonClick()
+            {
+                m_MessageInfo.m_Callback?.Invoke(MessageBox.ButtonID.Close, m_MessageInfo.m_Parameter);
+                OnClose();
+            }
+
+            private void OnClose()
+            {
+                m_MessageInfo.m_Destroying = true;
+                m_Canvas.enabled = false;
+            }
+        }
+    }
+}
